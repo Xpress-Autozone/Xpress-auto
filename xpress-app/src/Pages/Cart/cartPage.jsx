329aea976b446c3c9364bcb1b57f0a371cbbaa5c
godmode-dev <gokronipa@icloud.com>
2026-02-20 10:39:37 +0000

    cart redirects to xplore page

--- xpress-app/src/Pages/Cart/cartPage.jsx
+++ xpress-app/src/Pages/Cart/cartPage.jsx
@@ -52,7 +52,7 @@ export default function CartPage() {
               Start shopping to add items to your cart!
             </p>
             <button
-              onClick={() => navigate("/product")}
+              onClick={() => navigate("/xplore")}
               className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-6 rounded-lg transition-colors text-sm"
             >
               Continue Shopping
